9acc47eb52bae318cc9b2b5b365d70847a309080
Keler1024 <sergeydrozdov.1999@gmail.com>
2021-11-22 01:00:14 +0300

    Added option to load data from local database or files.

--- Analyzer/analyzer.py
+++ Analyzer/analyzer.py
@@ -25,9 +25,10 @@ def load_from_database(city, year):
 
 
 # Returns dictionary of requested weather parameters as keys and lists of year, month and mean value as values.
-# period can be "months" or "years", day_time can be "days" or "nights",
+# period can be "months" or "years", day_time can be "days", "nights" or "all",
 # weather_params is list with possible values "wind", "temperature" and "pressure"
-def average_values(city, period, day_time, start_year=1900, end_year=2100, weather_params=None, save_dir=None):
+def average_values(city, period, day_time, start_year=1997, end_year=2021, weather_params=None, save_dir=None,
+                   data_source="database"):
     if weather_params is None:
         weather_params = []
     result = dict()
@@ -36,72 +37,79 @@ def average_values(city, period, day_time, start_year=1900, end_year=2100, weath
     pressure_results = []
     temperature_results = []
     wind_results = []
-    for root, dirs, files in os.walk(f"../JSONs/{city}"):
-        if not files:
-            raise ValueError('No files to read data from')
-        for file in files:
-            year = int(file[:4])
-            # print(f"\nYEAR {year}")
-            if start_year < year < end_year:
-                with open(f'../JSONs/{city}/{file}', 'r', encoding='utf-8') as f:
-                    data = json.load(f)
-                if period == "years":
-                    if None in data["months"]:
-                        continue
-                # print(data)
-                period_days = 0
-                mean_pressure = 0
-                mean_wind = 0
-                mean_temperature = 0
-                for month in data["months"]:
-                    if month:
-                        month_num = month["num"]
-                        # print(f"\nMonth {month_num}\n")
-                        if period == "months":
-                            period_days = 0
-                            mean_wind = 0
-                            mean_temperature = 0
-                            mean_pressure = 0
-                        for day in month["days"]:
-                            days += 1
-                            # Records with missing data are discarded, also checks for requested day_time
-                            if day["temp"] and day["temp"] != "—" and day["temp"] != "−"\
-                                    and day["press"] and day["press"] != "—" and day["press"] != "−"\
-                                    and day["wind"] and day["wind"] != "—" and day["press"] != "−"\
-                                    and (day_time == "days" and "." not in day["num"]
-                                         or day_time == "nights" and "." in day["num"]):
-                                period_days += 1
-                                # print(day)
-                                if "wind" in weather_params or not weather_params:
-                                    if day["wind"] != "Ш":
-                                        mean_wind += int(re.search(r'\d+', day["wind"]).group())
-                                if "temperature" in weather_params or not weather_params:
-                                    mean_temperature += int(day["temp"])
-                                if "pressure" in weather_params or not weather_params:
-                                    mean_pressure += int(day["press"])
-                            else:
-                                discarded += 1
-                        if period == "months":
-                            if "pressure" in weather_params or not weather_params:
-                                mean_pressure /= period_days
-                                pressure_results.append((year, month_num, mean_pressure))
-                            if "temperature" in weather_params or not weather_params:
-                                mean_temperature /= period_days
-                                temperature_results.append((year, month_num, mean_temperature))
-                            if "wind" in weather_params or not weather_params:
-                                mean_wind /= period_days
-                                wind_results.append((year, month_num, mean_wind))
-                if period == "years":
-                    # print(f"Year {year}, {year_days} days")
+    # for root, dirs, files in os.walk(f"../JSONs/{city}"):
+    #     if not files:
+    #         raise ValueError('No files to read data from')
+    #     for file in files:
+    #         year = int(file[:4])
+    #         # print(f"\nYEAR {year}")
+    #         if start_year < year < end_year:
+    #             with open(f'../JSONs/{city}/{file}', 'r', encoding='utf-8') as f:
+    #                 data = json.load(f)
+    for year in range(start_year, end_year + 1):
+        if data_source == "database":
+            data = load_from_database(city, year)
+        elif data_source == "files":
+            data = load_from_files(city, year)
+        else:
+            raise ValueError('Invalid argument for argument "data_source" was provided')
+        if period == "years":
+            if None in data["months"]:
+                continue
+        # print(data)
+        period_days = 0
+        mean_pressure = 0
+        mean_wind = 0
+        mean_temperature = 0
+        for month in data["months"]:
+            if month:
+                month_num = month["num"]
+                # print(f"\nMonth {month_num}\n")
+                if period == "months":
+                    period_days = 0
+                    mean_wind = 0
+                    mean_temperature = 0
+                    mean_pressure = 0
+                for day in month["days"]:
+                    days += 1
+                    # Records with missing data are discarded, also checks for requested day_time
+                    if day["temp"] and day["temp"] != "—" and day["temp"] != "−"\
+                            and day["press"] and day["press"] != "—" and day["press"] != "−"\
+                            and day["wind"] and day["wind"] != "—" and day["press"] != "−"\
+                            and (day_time == "days" and "." not in day["num"]
+                                 or day_time == "nights" and "." in day["num"] or day_time == "all"):
+                        period_days += 1
+                        # print(day)
+                        if "wind" in weather_params or not weather_params:
+                            if day["wind"] != "Ш":
+                                mean_wind += int(re.search(r'\d+', day["wind"]).group())
+                        if "temperature" in weather_params or not weather_params:
+                            mean_temperature += int(day["temp"])
+                        if "pressure" in weather_params or not weather_params:
+                            mean_pressure += int(day["press"])
+                    else:
+                        discarded += 1
+                if period == "months":
                     if "pressure" in weather_params or not weather_params:
                         mean_pressure /= period_days
-                        pressure_results.append((year, mean_pressure))
+                        pressure_results.append((year, month_num, mean_pressure))
                     if "temperature" in weather_params or not weather_params:
                         mean_temperature /= period_days
-                        temperature_results.append((year, mean_temperature))
+                        temperature_results.append((year, month_num, mean_temperature))
                     if "wind" in weather_params or not weather_params:
                         mean_wind /= period_days
-                        wind_results.append((year, mean_wind))
+                        wind_results.append((year, month_num, mean_wind))
+        if period == "years":
+            # print(f"Year {year}, {year_days} days")
+            if "pressure" in weather_params or not weather_params:
+                mean_pressure /= period_days
+                pressure_results.append((year, mean_pressure))
+            if "temperature" in weather_params or not weather_params:
+                mean_temperature /= period_days
+                temperature_results.append((year, mean_temperature))
+            if "wind" in weather_params or not weather_params:
+                mean_wind /= period_days
+                wind_results.append((year, mean_wind))
     if "pressure" in weather_params or not weather_params:
         result["pressure"] = pressure_results
     if "temperature" in weather_params or not weather_params:
@@ -118,6 +126,7 @@ def average_values(city, period, day_time, start_year=1900, end_year=2100, weath
     return result
 
 
-average_values("Санкт-Петербург", "months", "nights")  # , save_dir="/JSONs/Results/2nd task/")
+# print(load_from_database("Санкт-Петербург", 2020))
+average_values("Санкт-Петербург", "years", "nights")  # , save_dir="/JSONs/Results/2nd task/")
 # for root, dirs, files in os.walk(f"../JSONs/Санкт-Петербург"):
 #     print(dirs)
